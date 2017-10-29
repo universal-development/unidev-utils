6c1029f24daada7ecdd8d495ffe42aeaf0c4cbb2
Denis O <denis@universal-development.com>
2017-10-29 19:40:57 +0200

    worldcoin index parser template

---
+++ worldcoinindex/worldcoinindex.groovy
@@ -0,0 +1,43 @@
+import com.unidev.platform.common.utils.RandomUtils
+import com.unidev.platform.common.utils.StringUtils
+import com.unidev.platform.components.http.HTTPClient
+import com.unidev.platform.components.http.HTTPClientUtils
+import com.unidev.platform.components.http.XTrustProvider
+import org.springframework.context.support.ClassPathXmlApplicationContext
+
+/**
+ * Copyright (c) 2017 Denis O <denis@universal-development.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * worldcoinindex parser
+ */
+
+
+@GrabResolver(name = 'decaf', root = 'http://ci.decafdev.local/nexus/content/groups/public')
+@Grab('com.unidev.platform.spring:simple-config:2.0.0-SNAPSHOT')
+@Grab('com.unidev.platform.components:http-client:2.0.0-SNAPSHOT')
+
+def ctx = new ClassPathXmlApplicationContext("classpath:/unidev-simple.xml");
+
+StringUtils stringUtils = ctx.getBean(StringUtils.class)
+
+println ctx
+
+XTrustProvider.install()
+
+HTTPClient httpClient = ctx.getBean(HTTPClient.class);
+httpClient.init(HTTPClientUtils.USER_AGENTS)
+println httpClient
